5fd2204e4b0fe39570d359b8518c666374a62975
Jiwoo Joy Kim <zuzokim@gmail.com>
2021-09-18 20:57:57 +0900

    numIsland

--- matrix/numIsland.js
+++ matrix/numIsland.js
@@ -14,28 +14,28 @@
 // Output:1
 
 const numIsland = (grid) => {
-   let height = grid.length;
-   let width = grid[0].length;
-   let count = 0;
-   for(let h =0; h < height; h++){
-     for(let w = 0; w < width; w++){
-       if(grid[h][w] === 0) continue;
-       count++;
-       searchIsland(h, w);
-     }
-   }
+  let height = grid.length;
+  let width = grid[0].length;
+  let count = 0;
+  for(let h =0; h < height; h++){
+    for(let w = 0; w < width; w++){
+      if(grid[h][w] === 0) continue;
+      count++;
+      searchIsland(h, w);
+    }
+  }
 
-   function searchIsland(h ,w){
-     if(h < 0 || w < 0 || w >= width || h >= height) return;
-     if(grid[h][w] === 0)return;
+  function searchIsland(h ,w){
+    if(h < 0 || w < 0 || w >= width || h >= height) return;
+    if(grid[h][w] === 0)return;
 
-     grid[h][w] = 0;
-     searchIsland(h-1, w);
-     searchIsland(h, w-1);
-     searchIsland(h+1, w);
-     searchIsland(h, w+1);
-   }
-   return count;
+    grid[h][w] = 0;
+    searchIsland(h-1, w);
+    searchIsland(h, w-1);
+    searchIsland(h+1, w);
+    searchIsland(h, w+1);
+  }
+  return count;
 }
 
 
